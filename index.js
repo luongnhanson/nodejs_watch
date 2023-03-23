7f81953e1d29baf135316a138896b544e46a6f82
luongnhanson <97085082+luongnhanson@users.noreply.github.com>
2023-03-23 20:47:50 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const morgan = require('morgan')
+
+
+const app = express()
+const port = 3000
+
+// morgan init
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
